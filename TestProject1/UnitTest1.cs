f87f8d90c2763ec28b9b06ebc3ec740cb79a8a88
User <User@WINDEV2208EVAL>
2022-11-09 03:27:22 -0800

    stored procedure working

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -26,61 +26,61 @@ namespace TestProject1
 
                     connection.Open();
 
+                    //CreateCustomerSchema(connection);
+                    //CreateReportSchema(connection);
+                    //CreateReportSchema1(connection);
+                    SendQbXml(connection);
+
+
+
+                    //using (IDataReader reader = SchemaCommand.ExecuteReader())
+                    //{
+                    //    var data = reader.GetSchemaTable();
+
+                    //}                    ///
                     var accountsCommand = connection.CreateCommand();
+                   
+                    
+                    
+                    //Debug.WriteLine(accountsCommand.Parameters["@OutputRawResponse"].Value);
+                    //accountsCommand.CommandText = $"EXECUTE SendQBXML";
+                    //accountsCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                    //var xml = File.ReadAllText("ConfigReport.xml");
+                    //accountsCommand.Parameters.Add("@RawXML", DbType.String).Value = xml;
 
-                    var SelectCustomersCommand = connection.CreateCommand();
-                    SelectCustomersCommand.CommandText = "Select * from CUSTOMERS";
+                    //QuickBooksParameter outparameter = new QuickBooksParameter("@OutputRawResponse", DbType.String);
+                    //ReportType.Direction = ParameterDirection.Output;
 
+                    //accountsCommand.Parameters.Add(ReportType);
 
-                    using (IDataReader reader = SelectCustomersCommand.ExecuteReader())
-                    {
-                        var data = reader.GetSchemaTable();
-                        //while (reader.Read())
-                        //{
+                    ////QuickBooksParameter outparameter = accountsCommand.Parameters.Add("@OutputRawResponse", SqlDbType.Text);
+                    ////outparameter.Direction = ParameterDirection.Output;
+                    //int affected = accountsCommand.ExecuteNonQuery();
 
-                        //    //read in data
-                        //}
+                    //var outvalue=  outparameter.Value.ToString();
 
-                        accountsCommand.CommandText = $"EXECUTE SendQBXML";
-                        accountsCommand.CommandType = System.Data.CommandType.StoredProcedure;
-                        var xml = File.ReadAllText("ConfigReport.xml");
-                        accountsCommand.Parameters.Add("@RawXML", DbType.String).Value = xml;
+                    //using (IDataReader reader = accountsCommand.ExecuteReader())
+                    //{
+                    //    var data = reader.GetSchemaTable();
+                    //    while (reader.Read())
+                    //    {
 
-                        QuickBooksParameter outparameter = new QuickBooksParameter("@OutputRawResponse", DbType.String);
-                        outparameter.Direction = ParameterDirection.Output;
+                    //        //read in data
+                    //    }
 
-                        accountsCommand.Parameters.Add(outparameter);
+                    //}
+                    //SqlParameter outputIdParam = new SqlParameter("@ID", SqlDbType.Int)
+                    //{
+                    //    Direction = ParameterDirection.Output
+                    //};
 
-                        //QuickBooksParameter outparameter = accountsCommand.Parameters.Add("@OutputRawResponse", SqlDbType.Text);
-                        //outparameter.Direction = ParameterDirection.Output;
-                        int affected = accountsCommand.ExecuteNonQuery();
-
-                        Debug.WriteLine(accountsCommand.Parameters["@OutputRawResponse"].Value);
-                        //var outvalue=  outparameter.Value.ToString();
-
-                        //using (IDataReader reader = accountsCommand.ExecuteReader())
-                        //{
-                        //    var data = reader.GetSchemaTable();
-                        //    while (reader.Read())
-                        //    {
-
-                        //        //read in data
-                        //    }
-
-                        //}
-                        //SqlParameter outputIdParam = new SqlParameter("@ID", SqlDbType.Int)
-                        //{
-                        //    Direction = ParameterDirection.Output
-                        //};
-
-                        //QuickBooksDataReader rdr = accountsCommand.ExecuteReader();
-                        //int currentRecord = 0;
-                        //while (rdr.Read())
-                        //{
+                    //QuickBooksDataReader rdr = accountsCommand.ExecuteReader();
+                    //int currentRecord = 0;
+                    //while (rdr.Read())
+                    //{
 
 
-                        //}
-                    }
+                    //}
                 }
                 catch (Exception ex)
                 {
@@ -91,6 +91,60 @@ namespace TestProject1
 
             }
             Assert.Pass();
+        }
+        void CreateReportSchema(QuickBooksConnection connection)
+        {
+            ///get schema
+            var SchemaCommand = connection.CreateCommand();
+            SchemaCommand.CommandText = $"EXECUTE CreateReportSchema";
+            SchemaCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+            QuickBooksParameter ReportType = new QuickBooksParameter("@ReportType", DbType.String);
+            ReportType.Direction = ParameterDirection.Output;
+            ReportType.Value = "BALANCESHEETSTANDARD";
+            SchemaCommand.Parameters.Add(ReportType);
+            var Reader = SchemaCommand.ExecuteReader();
+            var SchemaTable = Reader.GetSchemaTable();
+            //var affectedschema = SchemaCommand.ExecuteNonQuery();
+        }
+        void CreateReportSchema1(QuickBooksConnection connection)
+        {
+            ///get schema
+            var SchemaCommand = connection.CreateCommand();
+            SchemaCommand.CommandText = $"EXECUTE CreateReportSchema @ReportType = 'BALANCESHEETSTANDARD'";
+            SchemaCommand.CommandType = System.Data.CommandType.Text;
+            var Reader= SchemaCommand.ExecuteReader();
+            var SchemaTable= Reader.GetSchemaTable();
+            SchemaTable.WriteXml("BALANCESHEETSTANDARD.xml");
+            //var affectedschema = SchemaCommand.ExecuteNonQuery();
+        }
+        void SendQbXml(QuickBooksConnection connection)
+        {
+            ///get schema
+            var SchemaCommand = connection.CreateCommand();
+            var xml = File.ReadAllText("ConfigReport.xml");
+            SchemaCommand.CommandText = $"EXECUTE CreateReportSchema @SendQBXML = '{xml}'";
+            SchemaCommand.CommandType = System.Data.CommandType.Text;
+            var affected = SchemaCommand.ExecuteNonQuery();
+            var output=SchemaCommand.Parameters["OutputRawResponse"];
+            //Debug.WriteLine(accountsCommand.Parameters["@OutputRawResponse"].Value);
+            //accountsCommand.CommandText = $"EXECUTE SendQBXML";
+            //accountsCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            //var xml = File.ReadAllText("ConfigReport.xml");
+            //accountsCommand.Parameters.Add("@RawXML", DbType.String).Value = xml;
+            //var affectedschema = SchemaCommand.ExecuteNonQuery();
+        }
+        private static void CreateCustomerSchema(QuickBooksConnection connection)
+        {
+            var SelectCustomersCommand = connection.CreateCommand();
+            SelectCustomersCommand.CommandText = "Select * from CUSTOMERS";
+
+
+            using (IDataReader reader = SelectCustomersCommand.ExecuteReader())
+            {
+                var data = reader.GetSchemaTable();
+
+            }
         }
     }
 }
